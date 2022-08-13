cc83deaace479030d42384eea7456399d905bb6d
Vince Nguyen <ntgv1993@gmail.com>
2022-08-13 17:40:50 -0500

    updated eslint to prefer single quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
 
-
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div className='App'>
+    Hello World!
+  </div>
+);
 
 export default App;
