4cd0ef07f402f1deef6d8aeeed872df38bc00c80
Vince Nguyen <ntgv1993@gmail.com>
2022-08-13 17:17:06 -0500

    Added eslint rule, specified type for root App()

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-      </header>
+      Hello World!
     </div>
   );
 }
