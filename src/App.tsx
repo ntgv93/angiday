6a8db0f50a5c2f0a174b7fee4432999319117920
Vince Nguyen <ntgv1993@gmail.com>
2022-08-14 15:03:56 -0500

    Created common IngredientDropdown component for input dropdowns

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './App.css';
+import TextField from '@mui/material/TextField';
+import IngredientDropdown from './common/IngredientDropdown';
 
 const App: React.FC = () => (
   <div className='App'>
-    Hello World!
+    <h1>
+      Ăn Gì Đây
+    </h1>
+
+    {/* input section */}
+    <div>
+      <TextField label='Dish name' variant='outlined' />
+      {/* TODO:
+        - store ingredientType + ingredients in the DB
+        - fetch ingredientType + ingredients from the DB on load
+       */}
+      <IngredientDropdown ingredientType='Protein' ingredients={['Beef', 'Chicken', 'Pork', 'Sea food']} />
+      <IngredientDropdown ingredientType='Carb' ingredients={['Rice', 'Pasta', 'Potato', 'Bread']} />
+      <IngredientDropdown ingredientType='Veggies' ingredients={['Rau Muống', 'Broccoli', 'Spinach', 'Lettuce']} />
+    </div>
   </div>
 );
 
---
+++ src/common/IngredientDropdown.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from '@mui/material';
+
+interface Props {
+  ingredientType: string,
+  ingredients: string[]
+}
+
+const IngredientDropdown: React.FC<Props> = ({
+  ingredientType,
+  ingredients,
+}: Props) => {
+  const a = '';
+  return (
+    <FormControl>
+      <InputLabel>{ingredientType}</InputLabel>
+      <Select
+        label='Protein'
+        variant='outlined'
+      >
+        {ingredients.map((ingredient) => <MenuItem value={ingredient}>{ingredient}</MenuItem>)}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default IngredientDropdown;
